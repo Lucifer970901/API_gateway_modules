c898b5d7fe99875fcf1d27875110a90fb9b030a4
Meghashree N <meghashreetendulkar@gmail.com>
2023-02-13 13:41:35 +0530

    Update apiDescription.tf

--- apiDescription.tf
+++ apiDescription.tf
@@ -1,14 +1,19 @@
 data "local_file" "api_description_file" {
-    filename = "./fieldservice_api.yaml"
+    filename = "./openapi.yaml"
 }
 
 locals {
     api_description = yamldecode(data.local_file.api_description_file.content)
 }
 
+### Compartment
+variable "compartment_id" {
+    type = string
+}
 resource "oci_apigateway_api" "api_resource" {
+    for_each = fileset("${path.module}/../openapi", "*.yaml")
     compartment_id = var.compartment_id
-    content = data.local_file.api_description_file.content
-    display_name = local.api_description.info.title
-    
+    content = file("${path.module}/../openapi/${each.value}")
+    display_name = yamldecode(file("${path.module}/../openapi/${each.value}")).info.title
+   
 }
