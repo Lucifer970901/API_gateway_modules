a2cf86079aa2232c5f72a2c6f5f509609be6172e
Meghashree N <meghashreetendulkar@gmail.com>
2023-02-13 17:00:27 +0530

    Update api_Gateway.tf

--- api_Gateway.tf
+++ api_Gateway.tf
@@ -6,3 +6,12 @@ resource "oci_apigateway_gateway" "test_gateway" {
     #endpoint_type = var.endpoint_type
     subnet_id = var.subnet_id
 }
+
+module "gateway" {
+    source = "."
+
+    compartment_id = var.compartment_id
+    display_name = var.display_name
+    endpoint_type = var.endpoint_type
+    subnet_id = var.subnet_id
+}
