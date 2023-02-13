e7fd566acdccad94a635ccea9be1473234c2fa64
Meghashree N <meghashreetendulkar@gmail.com>
2023-02-13 15:26:52 +0530

    Update apiDescription.tf

--- apiDescription.tf
+++ apiDescription.tf
@@ -11,9 +11,9 @@ variable "compartment_id" {
     type = string
 }
 resource "oci_apigateway_api" "api_resource" {
-    for_each = fileset("${path.module}./openapi", "*.yaml")
+    for_each = fileset("${path.module}/openapi", "*.yaml")
     compartment_id = var.compartment_id
-    content = file("${path.module}./openapi/${each.value}")
-    display_name = yamldecode(file("${path.module}./openapi/${each.value}")).info.title
+    content = file("${path.module}/openapi/${each.value}")
+    display_name = yamldecode(file("${path.module}/openapi/${each.value}")).info.title
    
 }
