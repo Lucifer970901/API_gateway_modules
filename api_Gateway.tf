adf5b6fae628f1c6fe6507fb2cdeab6603b6b809
Meghashree N <meghashreetendulkar@gmail.com>
2023-02-13 13:33:02 +0530

    Create api_Gateway.tf

---
+++ api_Gateway.tf
@@ -0,0 +1,8 @@
+module "gateway" {
+    source = "../../infra/modules/gateway"
+
+    compartment_id = var.compartment_id
+    display_name = var.display_name
+    endpoint_type = var.endpoint_type
+    subnet_id = var.subnet_id
+}
