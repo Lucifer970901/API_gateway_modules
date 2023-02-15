ae0539b10f1b0bb77fbd4823c331f39c5685eb0f
Meghashree N <meghashreetendulkar@gmail.com>
2023-02-15 16:49:40 +0530

    Update api_Gateway.tf

--- api_Gateway.tf
+++ api_Gateway.tf
@@ -1,9 +1,7 @@
 module "test_gateway" {
    source = "./"
 
-    compartment_id = var.compartment_id
-    display_name = var.display_name
-    #endpoint_type = var.endpoint_type
-    subnet_id = var.subnet_id
+    display_name = "API_gateway"
+    
 }
 
