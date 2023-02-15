0989864c7d4c34055850ad049f7333c28af11bf2
Meghashree N <meghashreetendulkar@gmail.com>
2023-02-15 17:14:08 +0530

    Update gateway_resource.tf

--- modules/gateway_resource.tf
+++ modules/gateway_resource.tf
@@ -1,5 +1,5 @@
 
-resource "oci_apigateway_gateway" "test_gateway" {
+resource "oci_apigateway_gateway" "gateway" {
     #Required
     compartment_id = "${var.compartment_id}"
     endpoint_type = "${var.gateway_endpoint_type}"
