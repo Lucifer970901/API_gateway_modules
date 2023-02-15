cc307a23452075246ec98d992e2987d5a978f509
Meghashree N <meghashreetendulkar@gmail.com>
2023-02-15 16:34:43 +0530

    Update api_Gateway.tf

--- api_Gateway.tf
+++ api_Gateway.tf
@@ -1,5 +1,5 @@
-resource "oci_apigateway_gateway" "test_gateway" {
-   # source = "../../infra/modules/gateway"
+module "test_gateway" {
+   source = "./"
 
     compartment_id = var.compartment_id
     display_name = var.display_name
@@ -7,11 +7,3 @@ resource "oci_apigateway_gateway" "test_gateway" {
     subnet_id = var.subnet_id
 }
 
-module "gateway" {
-    source = "."
-
-    compartment_id = var.compartment_id
-    display_name = var.display_name
-    endpoint_type = var.endpoint_type
-    subnet_id = var.subnet_id
-}
