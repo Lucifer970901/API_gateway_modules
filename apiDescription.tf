b117598286b3b46d9f7f4a8fdf7e1b2f6ba60acc
Meghashree N <meghashreetendulkar@gmail.com>
2023-02-15 15:36:43 +0530

    Update apiDescription.tf

--- apiDescription.tf
+++ apiDescription.tf
@@ -16,6 +16,6 @@ resource "oci_apigateway_api" "api_resource" {
     for_each = fileset("${path.module}/openapi", "*.yaml")
     compartment_id = var.compartment_id
     content = file("${path.module}/openapi/${each.value}")
-    display_name = yamldecode(file("${path.module}/openapi/${each.value}")).info.title
-   
-}'''
+    display_name = yamldecode(file("${path.module}/openapi/${each.value}")).info.title  
+}
+'''
