8992515a0b3666152a544644fdfc5738c7ce1f82
Meghashree N <meghashreetendulkar@gmail.com>
2023-02-15 15:51:53 +0530

    Update apiDescription.tf

--- apiDescription.tf
+++ apiDescription.tf
@@ -6,16 +6,11 @@ locals {
     api_description = yamldecode(data.local_file.api_description_file.content)
 }
 
-resource "oci_apigateway_api" "api_resource" {
-    compartment_id = var.compartment_id
-    content = data.local_file.api_description_file.content
-    display_name = local.api_description.info.title
-}  
 
-#resource "oci_apigateway_api" "api_resource" {
- #   for_each = fileset("${path.module}/openapi", "*.yaml")
-  #  compartment_id = var.compartment_id
-  #  content = file("${path.module}/openapi/${each.value}")
-  #  display_name = yamldecode(file("${path.module}/openapi/${each.value}")).info.title  
-#}
+resource "oci_apigateway_api" "api_resource" {
+    for_each = fileset("${path.module}/openapi", "*.yaml")
+    compartment_id = var.compartment_id
+    content = file("${path.module}/openapi/${each.value}")
+    display_name = yamldecode(file("${path.module}/openapi/${each.value}")).info.title  
+}
 
