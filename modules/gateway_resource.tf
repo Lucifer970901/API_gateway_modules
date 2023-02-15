75f297474340e38a0324418c02a34f1c3910005a
Meghashree N <meghashreetendulkar@gmail.com>
2023-02-15 18:49:51 +0530

    Update gateway_resource.tf

--- modules/gateway_resource.tf
+++ modules/gateway_resource.tf
@@ -5,4 +5,6 @@ resource "oci_apigateway_gateway" "gateway" {
     endpoint_type = "${var.gateway_endpoint_type}"
     subnet_id = "${var.subnet_id}"
     display_name = "API_gateway"
+    timeouts {
+    create = "15m"}
 }
