53c8afd9b266d5d85bbc092958abde8acb097626
Meghashree N <meghashreetendulkar@gmail.com>
2023-02-15 16:48:12 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-module "gateway" {
-    source = "./"
-
-    compartment_id = var.compartment_id
-    display_name = var.display_name
-    endpoint_type = var.endpoint_type
-    subnet_id = var.subnet_id
-}
-
